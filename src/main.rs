43fedb49824023d38444de19dc8d2aba18c956df
Br3nnabee <brenna.conovali@protonmail.com>
2024-06-13 01:09:26 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,8 @@ struct Icon(String);
 #[derive(Component, Debug, Clone)]
 struct Tags(Vec<String>);
 
+//TODO: Find a more memory efficient structure for the below
+
 #[derive(Debug, Clone)]
 enum PropertyValue {
     Bool(bool),
@@ -47,52 +49,97 @@ struct RawItemData {
 #[derive(Resource)]
 struct ItemStorage {
     items: HashMap<Name, RawItemData>,
-}
+} //TODO: Test loading to and from disk
 
-fn spawn_sword(mut commands: Commands, item_storage: Res<ItemStorage>) {
-    let item_name = Name::new("sword".to_string());
-    if let Some(sword) = item_storage.items.get(&item_name) {
+fn spawn_item(mut commands: Commands, item_storage: Res<ItemStorage>, name: String) {
+    let item_name = Name::new(name);
+    if let Some(item) = item_storage.items.get(&item_name) {
         let mut entity = commands.spawn((Item, item_name.clone()));
 
-        if let Some(displayname) = &sword.displayname {
+        if let Some(displayname) = &item.displayname {
             entity.insert(displayname.clone());
         }
-        if let Some(weight) = &sword.weight {
+        if let Some(weight) = &item.weight {
             entity.insert(weight.clone());
         }
-        if let Some(use_delta) = &sword.use_delta {
+        if let Some(use_delta) = &item.use_delta {
             entity.insert(use_delta.clone());
             entity.insert(UseAmount(1.0));
         }
-        if let Some(icon) = &sword.icon {
+        if let Some(icon) = &item.icon {
             entity.insert(icon.clone());
         }
-        if let Some(properties) = &sword.properties {
+        if let Some(properties) = &item.properties {
             entity.insert(properties.clone());
         }
 
-        println!("Successfully spawned sword.");
+        println!("Successfully spawned {}.", &item_name);
     } else {
-        println!("Sword not found in item storage.");
+        println!("Item '{}' not found in item storage.", &item_name);
     }
 }
 
+fn spawn_sword(mut commands: Commands, item_storage: Res<ItemStorage>) {
+    let name = "potion".to_string();
+    spawn_item(commands, item_storage, name)
+}
+
 fn initialize_dictionary(mut commands: Commands, mut item_storage: ResMut<ItemStorage>) {
-    let item_name = Name::new("sword");
+    //TODO: Add deserialization and reading from msgpack
 
-    let mut properties = HashMap::new();
-    properties.insert(String::from("Damage"), PropertyValue::Int(30));
+    let items_data = vec![
+        (
+            "sword",
+            RawItemData {
+                displayname: Some(DisplayName("Sword".to_string())),
+                weight: Some(Weight(1.0)),
+                use_delta: None,
+                icon: None,
+                tags: None,
+                properties: Some(ItemProperties({
+                    let mut props = HashMap::new();
+                    props.insert(String::from("Damage"), PropertyValue::Int(30));
+                    props
+                })),
+            },
+        ),
+        (
+            "shield",
+            RawItemData {
+                displayname: Some(DisplayName("Shield".to_string())),
+                weight: Some(Weight(5.0)),
+                use_delta: None,
+                icon: None,
+                tags: None,
+                properties: Some(ItemProperties({
+                    let mut props = HashMap::new();
+                    props.insert(String::from("Defense"), PropertyValue::Int(50));
+                    props
+                })),
+            },
+        ),
+        (
+            "potion",
+            RawItemData {
+                displayname: Some(DisplayName("Potion".to_string())),
+                weight: Some(Weight(0.25)),
+                use_delta: Some(UseDelta(0.125)),
+                icon: Some(Icon("Icon".to_string())),
+                tags: Some(Tags(vec!["Healing".to_string(), "Consumable".to_string()])),
+                properties: None,
+            },
+        ),
+    ];
 
-    let item = RawItemData {
-        displayname: Some(DisplayName("Sword".to_string())),
-        weight: Some(Weight(1.0)),
-        use_delta: None,
-        icon: None,
-        tags: None,
-        properties: Some(ItemProperties(properties)),
-    };
+    for (name, data) in items_data {
+        let item_name = Name::new(name);
+        item_storage.items.insert(item_name, data);
+    }
 
-    item_storage.items.insert(item_name, item);
+    println!(
+        "Item dictionary initialized with {} items.",
+        item_storage.items.len()
+    );
 }
 
 fn fetch_item_info(
@@ -143,9 +190,22 @@ fn fetch_item_info(
         }
 
         if let Some(itemproperties) = itemproperties_option {
-            println!("    Properties: {:?}", itemproperties.0);
+            println!("    Properties:");
+            for (key, value) in &itemproperties.0 {
+                match value {
+                    PropertyValue::Bool(val) => println!("      {}: {}", key, val),
+                    PropertyValue::Int(val) => println!("      {}: {}", key, val),
+                    PropertyValue::Float(val) => println!("      {}: {}", key, val),
+                    PropertyValue::Text(val) => println!("      {}: {}", key, val),
+                }
+            }
         }
     }
+}
+
+fn test_spawn_nonexistent_item(mut commands: Commands, item_storage: Res<ItemStorage>) {
+    let name = "nonexistent_item".to_string();
+    spawn_item(commands, item_storage, name)
 }
 
 fn main() {
@@ -155,6 +215,6 @@ fn main() {
             items: HashMap::new(),
         })
         .add_systems(Startup, (initialize_dictionary, spawn_sword))
-        .add_systems(PostStartup, fetch_item_info)
+        .add_systems(PostStartup, (fetch_item_info, test_spawn_nonexistent_item))
         .run();
 }
